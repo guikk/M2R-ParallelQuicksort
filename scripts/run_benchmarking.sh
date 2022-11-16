05a9b0841964fd2459e696078b85802eea81b140
Guilherme Klein Kern <guilherme.klein-kern@grenoble-inp.org>
2022-11-16 12:37:23 +0100

    Add code and measurements from class

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -1,12 +1,15 @@
 
 OUTPUT_DIRECTORY=data/`hostname`_`date +%F`
 mkdir -p $OUTPUT_DIRECTORY
-OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements_`date +%R`.txt
+FILENAME=$OUTPUT_DIRECTORY/measurements_`date +%R`
 
-touch $OUTPUT_FILE
-for i in 100 1000 10000 100000 1000000; do
+touch $FILENAME.txt
+for i in 100 100000 200000 300000 400000 500000 600000 700000 800000 900000 1000000; do
     for rep in `seq 1 5`; do
-        echo "Size: $i" >> $OUTPUT_FILE;
-        ./src/parallelQuicksort $i >> $OUTPUT_FILE;
+        echo "Size: $i" >> $FILENAME.txt;
+        ./src/parallelQuicksort $i >> $FILENAME.txt;
     done ;
 done
+
+perl scripts/csv_quicksort_extractor.pl < $FILENAME.txt > $FILENAME.csv
+perl scripts/csv_quicksort_extractor2.pl < $FILENAME.txt > $FILENAME\_wide.csv
